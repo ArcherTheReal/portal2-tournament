65a81a61a1145a38ea76a7ca52f0e34fcc51dec0
Archer <62026087+ArcherTheReal@users.noreply.github.com>
2024-07-20 00:03:21 +0200

    init

---
+++ main.ts
@@ -0,0 +1,17 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+const fetchHandler = async function (req: Request) {
+  const url = new URL(req.url);
+  const urlPath = url.pathname.split("/").slice(1);
+  const userAgent = req.headers.get("User-Agent");
+
+  return new Response("temp");
+};
+
+const server = Bun.serve({
+  port: 3000,
+  fetch: fetchHandler,
+});
+
+console.log("Server running on port 3000");
