dcb0e2f36039c8568f106c933e36e58a8cbb7be1
Archer <62026087+ArcherTheReal@users.noreply.github.com>
2024-07-20 20:20:00 +0200

    feat: steam auth backend

---
+++ api/auth.ts
@@ -0,0 +1,65 @@
+const fs = require("node:fs");
+const path = require("node:path");
+const jwt = require("jsonwebtoken");
+const SteamAuth = require("../steamauth");
+
+const steam = new SteamAuth({
+  realm: "http://192.168.72.1:3000/",
+  returnUrl: "http://192.168.72.1:3000/api/auth/return/",
+  apiKey: global.tournament.keys.steam,
+});
+
+declare global {
+  var getUserToken: (req: Request) => any;
+}
+
+global.getUserToken = function (req: Request) {
+  try {
+    const cookie = req.headers.get("Cookie");
+    if (!cookie) return null;
+
+    const cookies = cookie.split(";").map((c) => c.trim());
+    const token = cookies.find((c) => c.startsWith("steamtoken="));
+    if (!token) return null;
+
+    return jwt.verify(token.split("=")[1], global.tournament.keys.jwt);
+  } catch (e) {
+    return null;
+  }
+};
+
+module.exports = async function (args: any[string], req: Request) {
+  if (req.method === "GET") {
+    if (args.length === 0) {
+      return new Response("No action specified", { status: 400 });
+    }
+
+    const action = args[0];
+
+    switch (action) {
+      case "login":
+        const url = await steam.getRedirectUrl();
+        return Response.redirect(url, 302);
+
+      case "return":
+        const user = await steam.authenticate(req);
+        console.log(user);
+        const token = jwt.sign(user, global.tournament.keys.jwt);
+        const headers = new Headers({
+          "Set-Cookie": `steamtoken=${token}; Path=/; HttpOnly; Max-Age=86400`,
+          Location: "/",
+        });
+        return new Response(null, { headers, status: 302 });
+
+      case "logout":
+        const newHeaders = new Headers({
+          "Set-Cookie": "steamtoken=; Path=/; HttpOnly; Max-Age=0",
+          Location: "/",
+        });
+        return new Response(null, { headers: newHeaders });
+
+      default:
+        return new Response("Invalid action", { status: 400 });
+    }
+  }
+};
